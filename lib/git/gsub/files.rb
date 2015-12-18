89cb35a22bd3d3f1465e8370d142c445498609b0
k1LoW <k1lowxb@gmail.com>
2015-12-18 21:36:33 +0900

    Add verbose

--- lib/git/gsub/files.rb
+++ lib/git/gsub/files.rb
@@ -27,7 +27,7 @@ module Git
         (`git ls-files #{path}`).each_line.map(&:chomp).map do |file|
           next unless File.basename(file).match(from)
           to_file = File.dirname(file) + '/' + File.basename(file).gsub(from, to)
-          system %|mv #{file} #{to_file}|
+          system %|mv -v #{file} #{to_file}|
         end
       end
     end
